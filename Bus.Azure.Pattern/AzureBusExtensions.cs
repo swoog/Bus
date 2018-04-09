e34dd4d4b9a34763b3ad4be0bf8007d4aabc385b
Aurélien GALTIER <aurelien.galtier@live.fr>
2018-04-09 14:10:06 +0200

    Add AzureBus pattern project

---
+++ Bus.Azure.Pattern/AzureBusExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bus.AzureBus;
+using Bus.Core;
+using Pattern.Config;
+using Pattern.Core.Interfaces;
+
+namespace Bus.Azure.Pattern
+{
+    public static class AzureBusExtensions
+    {
+        public static IKernel BindAzureBus(this IKernel kernel, string connectionString, string storageAccount, string name)
+        {
+            kernel.Bind<EventHubClientFactory>().ToMethod(() => new EventHubClientFactory(connectionString));
+            kernel.Bind<EventProcessorHostFactory>().ToMethod(() => new EventProcessorHostFactory(connectionString, storageAccount, name));
+            kernel.Bind<IBus>().To<AzureBus.AzureBus>();
+
+            return kernel;
+        }
+    }
+}
